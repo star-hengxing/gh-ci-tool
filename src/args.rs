25c674331f52aa569983ee3d59a0cfb8fd054eb9
star9029 <hengxings783@gmail.com>
2026-02-27 22:16:09 +0800

    feat: enable clap default version flag

--- src/args.rs
+++ src/args.rs
@@ -3,6 +3,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(name = "gh-ci-tool")]
 #[command(about = "Check GitHub Actions CI status for current commit")]
+#[command(version)]
 pub struct Args {
     /// Disable log download for failed jobs
     #[arg(long, default_value_t = false)]
